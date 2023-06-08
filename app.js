a877cf0122017374637f6da7ed3a2887b1153ba1
Linh Nguyen <linhtunguyen.1311@gmail.com>
2023-06-08 22:38:17 +0930

    mysql2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var session = require('express-session');
 
 var indexRouter = require('./routes/index');
